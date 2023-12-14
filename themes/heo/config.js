97efe310d4ff3740b4fdb1020d2525049497b097
wch770113 <70896544+wch770113@users.noreply.github.com>
2023-12-14 14:38:47 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -5,22 +5,22 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '初中数学小站欢迎您', url: 'https://661420.xyz' },
+    { title: '访问文档中心获取更多帮助', url: 'https://661420.xyz' }
   ],
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_1: '分享初中数学思想',
+  HEO_HERO_TITLE_2: '与提高初中数学学习能力',
+  HEO_HERO_TITLE_3: '661420.xyz',
+  HEO_HERO_TITLE_4: '欢迎大家',
+  HEO_HERO_TITLE_5: 'Mr.王哥欢迎你',
+  HEO_HERO_TITLE_LINK: 'https://661420.xyz',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
+  HEO_HERO_CATEGORY_1: { title: '原创文章', url: '/tag/原创文章' },
   HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_3: { title: '实用资源', url: '/tag/实用资源' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
@@ -29,15 +29,14 @@ const CONFIG = {
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   HEO_INFOCARD_GREETINGS: [
-    '你好！我是',
+    '你好！我是Mr.王哥',
     '🔍 分享与热心帮助',
-    '🤝 专修交互与设计',
-    '🏃 脚踏实地行动派',
-    '🏠 智能家居小能手',
-    '🤖️ 数码科技爱好者',
-    '🧱 团队小组发动机'
-  ],
-  HEO_INFO_CARD_URL: 'https://github.com/tangly1024/NotionNext', // 个人资料底部按钮链接
+    '🤝 专修初中数学',
+    '🏃 脚踏实地行动',
+    '🏠 为你解忧小能手',
+    '🤖️ 教师网络应用分享者',
+    ],
+  HEO_INFO_CARD_URL: 'https://661420.xyz', // 个人资料底部按钮链接
 
   // 用户技能图标
   HEO_GROUP_ICONS: [
@@ -103,7 +102,7 @@ const CONFIG = {
   HEO_SOCIAL_CARD_TITLE_1: '交流频道',
   HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
   HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  HEO_SOCIAL_CARD_URL: 'https://661420.xyz',
 
   // *****  以下配置无效，只是预留开发 ****
   // 菜单配置
